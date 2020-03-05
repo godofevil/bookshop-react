bfeec8d97120e6e386e8c8aa86df7c9b099ea0b2
godofevil <rkolodiazhniy@odessa.gov.ua>
2020-03-05 15:37:21 +0200

    first commit

---
+++ precache-manifest.8cc31a59a9b4c0cf6ce481cf3aae9ae6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e082e20ef6cbb2accd54282684c275ac",
+    "url": "/bookshop-react/index.html"
+  },
+  {
+    "revision": "1f20f2935e9cd273da27",
+    "url": "/bookshop-react/static/css/main.978b6652.chunk.css"
+  },
+  {
+    "revision": "e22d62f07578051e41a0",
+    "url": "/bookshop-react/static/js/2.aa2da7c2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/bookshop-react/static/js/2.aa2da7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f20f2935e9cd273da27",
+    "url": "/bookshop-react/static/js/main.a17a3580.chunk.js"
+  },
+  {
+    "revision": "0c1cbf7338335047fcea",
+    "url": "/bookshop-react/static/js/runtime-main.6a91bf5d.js"
+  },
+  {
+    "revision": "3d99fe52a2b797200463182d8363124f",
+    "url": "/bookshop-react/static/media/bg.3d99fe52.png"
+  },
+  {
+    "revision": "7b2438966e68322a30dd9299f92ee4f2",
+    "url": "/bookshop-react/static/media/close.7b243896.svg"
+  },
+  {
+    "revision": "c080a050af4b122c0ea132a49c906a0c",
+    "url": "/bookshop-react/static/media/facebook.c080a050.svg"
+  },
+  {
+    "revision": "a216bfe5b39a7b2a668a2f95cae0cc83",
+    "url": "/bookshop-react/static/media/instagram.a216bfe5.svg"
+  },
+  {
+    "revision": "f01f149ad8c26d549b527808d8188c00",
+    "url": "/bookshop-react/static/media/login.f01f149a.svg"
+  },
+  {
+    "revision": "9d4e18d889f5124f2a34adfd042d087e",
+    "url": "/bookshop-react/static/media/main.9d4e18d8.png"
+  },
+  {
+    "revision": "8432d4b7a01d12260b77e66088d7ae72",
+    "url": "/bookshop-react/static/media/menu.8432d4b7.svg"
+  },
+  {
+    "revision": "3b384b33f2d33bb888eae2443bce144b",
+    "url": "/bookshop-react/static/media/pinterest.3b384b33.svg"
+  },
+  {
+    "revision": "7c3064ca8632a4e64874dcee4c084cb0",
+    "url": "/bookshop-react/static/media/quotation.7c3064ca.png"
+  },
+  {
+    "revision": "7faf62c3236dda3f3dc96826b0272963",
+    "url": "/bookshop-react/static/media/search.7faf62c3.svg"
+  }
+]);
